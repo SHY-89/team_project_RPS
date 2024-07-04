165071ff12370ceb39a038c773794822f91510cc
SHY-89 <tjduwkrn@gmail.com>
2024-07-04 13:03:08 +0900

    app.py 가위바위보 부분 추가 및 결과 리턴, game.html에서 해당값 console 창에 출력

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from flask_cors import CORS
+import random
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,9 +9,27 @@ def games():
 
 @app.route('/game/winlose')
 def game_winlose():
-    player = request.args.get("user_choise")
-    
-    return player
+
+    user = request.args.get("user_choise")
+    # RPS 부분 추가
+    rps = {'1': '가위', '2': '바위', '3': '보'}
+    computer = ['1', '2', '3']
+    choice = random.choice(computer)
+    reuslt = ""
+    if user == choice:
+        reuslt = "비겼 습니다."
+    elif (choice == '1' or choice == '3') and (user == '1' or user == '3'):
+        if int(choice) % 3 > int(user) % 3:
+            reuslt = "컴퓨터 승리!!"
+        else:
+            reuslt = "사용자 승리!!"
+    else:
+        if int(choice) > int(user):
+            reuslt = "컴퓨터 승리!!"
+        else:
+            reuslt = "사용자 승리!!"
+
+    return {'computer' : rps[choice], 'reuslt': reuslt}
 
 
 if __name__ == '__main__':
