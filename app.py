d5aeff2f4b9ed62e61626ef18dcd16d04b81b4a0
SHY-89 <tjduwkrn@gmail.com>
2024-07-04 12:50:59 +0900

    game.html에 ajax 쪽 url_for 주소오류 수정 및 app.py data 받는지 확인

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from flask_cors import CORS
 app = Flask(__name__)
 
 @app.route('/')
 def games():
     return render_template('game.html')
+
+@app.route('/game/winlose')
+def game_winlose():
+    player = request.args.get("user_choise")
     
+    return player
+
+
 if __name__ == '__main__':
     app.run()
