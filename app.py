d4267db023ae52725880d755dbc389b4639e2ff2
SHY-89 <tjduwkrn@gmail.com>
2024-07-05 11:32:21 +0900

    session을 통해 login 기능 구현 완

--- app.py
+++ app.py
@@ -1,33 +1,53 @@
-from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask import Flask, render_template, url_for, request, session, redirect
 from flask_cors import CORS
 import random
 from datetime import datetime
 app = Flask(__name__)
 
 # DB 기본 코드
-import os
-from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.secret_key = 'lXiptP4htKFY6SemXVvxNxF4w'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+    'sqlite:///' + os.path.join(basedir, 'database.db')
 
 db = SQLAlchemy(app)
+
 
 class Users(db.Model):
     idx = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(100), nullable=False)
     user_name = db.Column(db.String(100), nullable=False)
     user_pw = db.Column(db.String(100), nullable=False)
-    join_date = db.Column(db.DateTime,default=datetime.now(), onupdate=datetime.now())
+    join_date = db.Column(
+        db.DateTime, default=datetime.now(), onupdate=datetime.now())
+
 
 @app.route('/')
 def games():
-    return render_template('game.html')
+    return_url = ''
+    if 'user_id' in session:
+        user_id = session['user_id']
+        # 전적 검색 부분 
+        return_url = 'game.html'
+    else:
+        return_url = 'login.html'
+    return render_template(return_url)
 
-@app.route('/login')
+@app.route('/login', methods=['POST'])
 def login():
+    if 'user_id' in session:
+        return render_template('/')
+    elif request.method == 'POST':
+        filter_list = Users.query.filter_by(user_id=request.form['user_id'],user_pw=request.form['user_pw']).all()
+        if filter_list:
+            session['user_id'] = request.form['user_id']
+            return redirect(url_for('games'))        
+        
     return render_template('login.html')
+
 
 @app.route('/sign')
 def sign():
@@ -36,7 +56,7 @@ def sign():
 
 @app.route('/check/user')
 def check_user():
-    ruser_id =  request.args.get("user_id")
+    ruser_id = request.args.get("user_id")
     filter_list = Users.query.filter_by(user_id=ruser_id).all()
     result = 'sussece'
     if filter_list:
@@ -48,19 +68,20 @@ def check_user():
 def user_create():
     params = request.form
     ruser_id = params['user_id']
-    ruser_pw =  params['user_pw']
-    ruser_name =  params['user_name']
+    ruser_pw = params['user_pw']
+    ruser_name = params['user_name']
     filter_list = Users.query.filter_by(user_id=ruser_id).all()
 
     result = 'sussece'
     if filter_list:
         result = 'fail'
     else:
-        user = Users(user_id=ruser_id, user_name = ruser_name, user_pw = ruser_pw)
+        user = Users(user_id=ruser_id, user_name=ruser_name, user_pw=ruser_pw)
         db.session.add(user)
         db.session.commit()
 
     return {'reuslt': result}
+
 
 @app.route('/game/winlose')
 def game_winlose():
@@ -84,7 +105,8 @@ def game_winlose():
         else:
             reuslt = "사용자 승리!!"
 
-    return {'computer' : rps[choice], 'reuslt': reuslt}
+    return {'computer': rps[choice], 'reuslt': reuslt}
+
 
 if __name__ == '__main__':
     app.run()
