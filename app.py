b98d504ad16efabd4fba9f25b38f93301c794b28
SHY-89 <tjduwkrn@gmail.com>
2024-07-08 20:41:56 +0900

    code 병합 부분 추가

--- app.py
+++ app.py
@@ -315,8 +315,8 @@ def game_cmh():
                            computer=computer_choice, result=result)
         db.session.add(new_record)
         db.session.commit()
-
-        return render_template('result.html', user_choice=user_choice, computer_choice=computer_choice, result=result)
+        print(form,{'user_choice':user_choice, 'computer_choice':computer_choice, 'result':result})
+        return render_template('game_cmh.html',form=form, result = {'user_choice':user_choice, 'computer_choice':computer_choice, 'result':result})
 
 
     return render_template('game_cmh.html', form=form)
